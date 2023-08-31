3b8a9ac1c52819335bcc5236b81bd083ef6de60e
Sujal Goel <gglebot.helper@gmail.com>
2023-09-01 04:56:32 +0530

    add credits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-// import Link from 'next/link';
+import Link from 'next/link';
 import 'tailwindcss/tailwind.css';
 import React, { useState } from 'react';
 import { ToastContainer } from 'react-toastify';
@@ -43,6 +43,17 @@ export default function HomePage(): JSX.Element {
 			<div className='mt-10 text-center animate-fade-in'>
 				<h2 className='text-sm text-zinc-500'>
 					Click on the text above to upload an image
+				</h2>
+
+				<h2 className='text-xs text-zinc-500'>
+					Front end elegantly tailored after{' '}
+					<Link
+						target='_blank'
+						href='https://chronark.com'
+						className='underline duration-500 hover:text-zinc-300'
+					>
+						chronark
+					</Link>
 				</h2>
 			</div>
 		</div>
